9a35f704ef6fa2b3717ac254880b08f974eb339a
Chris Engel <Chris.Engel@yahoo.com>
2020-07-14 00:25:36 -0400

    Updates

---
+++ precache-manifest.7a6dd7d2b3cb4b66056d9cdf00c71ad1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa874f05b7645e70ab60ee190c6dc680",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a950dcc49f27f16b9cdb",
+    "url": "/portfolio/static/css/main.e880da42.chunk.css"
+  },
+  {
+    "revision": "801cdba89cf6a9e6ace8",
+    "url": "/portfolio/static/js/2.89f7e11b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.89f7e11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a950dcc49f27f16b9cdb",
+    "url": "/portfolio/static/js/main.a499d5ef.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "88eeefc72a78654442e9e6884a200c17",
+    "url": "/portfolio/static/media/GameOfLife.88eeefc7.PNG"
+  },
+  {
+    "revision": "78f581932e8ec262759e96cd9c375900",
+    "url": "/portfolio/static/media/ProjectCanopy.78f58193.PNG"
+  },
+  {
+    "revision": "310c2306b913eb48ca414b8b691bc3cc",
+    "url": "/portfolio/static/media/Simmr.310c2306.PNG"
+  },
+  {
+    "revision": "428c5dacf49fed89618f90dcdbd2b877",
+    "url": "/portfolio/static/media/green_keyboard.428c5dac.jpg"
+  },
+  {
+    "revision": "d6f016ee528c495c608ef6bbcf3d80c7",
+    "url": "/portfolio/static/media/profile.d6f016ee.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0146a24d425d527960c75ad5cff16c35.js"
+  "/portfolio/precache-manifest.7a6dd7d2b3cb4b66056d9cdf00c71ad1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
