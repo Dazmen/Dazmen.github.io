c6f5de0f5799c585f60c357a1250171b757a1ea2
Chris Engel <Chris.Engel@yahoo.com>
2020-07-13 23:31:07 -0400

    Updates

---
+++ precache-manifest.0146a24d425d527960c75ad5cff16c35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acf3a45ad77d5c19f39bc73ac4d34e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a950dcc49f27f16b9cdb",
+    "url": "/static/css/main.e880da42.chunk.css"
+  },
+  {
+    "revision": "eefb1fcec480d87b4fbc",
+    "url": "/static/js/2.9eb43f8e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9eb43f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a950dcc49f27f16b9cdb",
+    "url": "/static/js/main.a499d5ef.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "88eeefc72a78654442e9e6884a200c17",
+    "url": "/static/media/GameOfLife.88eeefc7.PNG"
+  },
+  {
+    "revision": "78f581932e8ec262759e96cd9c375900",
+    "url": "/static/media/ProjectCanopy.78f58193.PNG"
+  },
+  {
+    "revision": "310c2306b913eb48ca414b8b691bc3cc",
+    "url": "/static/media/Simmr.310c2306.PNG"
+  },
+  {
+    "revision": "428c5dacf49fed89618f90dcdbd2b877",
+    "url": "/static/media/green_keyboard.428c5dac.jpg"
+  },
+  {
+    "revision": "d6f016ee528c495c608ef6bbcf3d80c7",
+    "url": "/static/media/profile.d6f016ee.jpg"
+  }
+]);
